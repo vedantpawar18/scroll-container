5515cd27d2b97b0355b55b0c4028186b715309aa
vedantpawar18 <vedantpawar18@gmail.com>
2022-07-02 20:19:31 +0530

    scroll to top on click

---
+++ src/components/ScrollContainer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useRef } from 'react';
+
+const ScrollContainer = () => {
+    const ref = useRef(null);
+    const handleClick = () => {
+      ref.current.scrollTop = 0;
+    };
+    const data = new Array(100).fill(0).map((item, i) => <li key={i}>{i}</li>);
+    return (
+      <div>
+        <div
+          ref={ref}
+          style={{
+            border: "1px solid black",
+            height: "200px",
+            overflowY: "scroll"
+          }}
+        >
+          <ul>{data}</ul>
+        </div>
+        <button onClick={handleClick}>Scroll to Top</button>
+      </div>
+  )
+}
+
+export default ScrollContainer
